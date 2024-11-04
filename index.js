9ab6b46449091ed877ac001107c306362f3367ca
Gary Li <gary.li@sap.com>
2024-11-04 15:31:18 +0800

    by #5   00:39:09 Type conversion

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-let price = 10.99;
-let myname = 'Hiking Boots';
-document.getElementById('L1H1').textContent = 'Hello World!';
-document.getElementById('L1P').innerHTML = 
-    `This is a paragraph to show the price: $${price} 
-    <br>
-    and the type of name: ${typeof myname}`;
+let outputHTML;
+let username, age;
 
-     document.getElementById('L1P').textContent += 
-        `\n
-        Line 1: This is the first line.
-        \n
-        Line 2: This is the second line.
-        \n
-        Line 3: This is the third line.`;
-        document.getElementById('L1P').innerHTML = document.getElementById('L1P').textContent.replace(/\n/g, '<br>');
+let pElement = document.createElement('p');
+pElement.id = 'L1P';
+document.body.appendChild(pElement);
+
+document.getElementById('L1Button').onclick = function() {
+    onclickL1Button();
+}
+
+function onclickL1Button() {
+    outputHTML = `This is a dynamically created UI element to show: <br>`;
+    username = document.getElementById('L1Username').value;
+    outputHTML += `Hello ${username}, the type of the input is ${typeof username}.`;
+
+    age = Number(document.getElementById('L1Age').value);
+    outputHTML += `<br>Your age is:  ${age}, the type of the input is ${typeof age}.`;
+    outputHTML += `<br> type of String(age) age is:  ${typeof(String(age))}`;
+    outputHTML += `<br> type of Boolean(age) age is:  ${typeof(Boolean(age))}`;
+
+    document.getElementById('L1P').innerHTML = outputHTML; 
+}
+
