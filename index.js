95e4597a07df7a24e929f7ea5d45423267e58cc1
Gary Li <gary.li@sap.com>
2024-11-05 10:32:16 +0800

    #8   01:01:46 Math object 🧮

--- index.js
+++ index.js
@@ -45,6 +45,15 @@ function onclickL1Button() {
     outputHTML += `<br>Your age is:  ${age}, the type of the input is ${typeof age}.`;
     outputHTML += `<br> type of String(age) age is:  ${typeof(String(age))}`;
     outputHTML += `<br> type of Boolean(age) age is:  ${typeof(Boolean(age))}`;
+    outputHTML += `<br> Math.PI is:  ${Math.PI}` ;
+    outputHTML += `<br> Math.round(Math.PI) is:  ${Math.round(Math.PI)}` ;
+    outputHTML += `<br> Math.floor(Math.PI) is:  ${Math.floor(Math.PI)}` ;
+    outputHTML += `<br> Math.ceil(Math.PI) is:  ${Math.ceil(Math.PI)}` ;
+    outputHTML += `<br> Math.trunc(Math.PI) is:  ${Math.trunc(Math.PI)}` ;
+    outputHTML += `<br> Math.pow(2,3) is:  ${Math.pow(2,3)}` ;
+    outputHTML += `<br> Math.sqrt(2) is:  ${Math.sqrt(2)}` ;    
+    outputHTML += `<br> Math.abs(-2) is:  ${Math.abs(-2)}` ;    
+    outputHTML += `<br> Math.sign(-3.14159) is:  ${Math.sign(-3.14159)}` ; 
 
     document.getElementById('L1P').innerHTML = outputHTML; 
 }
